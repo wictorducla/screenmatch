bdde4b03a53f7e7ae36bfe51e755ce24191b9d94
Wictor Ducla <wictor.ducla14@gmail.com>
2024-03-08 14:38:17 -0300

    ajustando a gravação dos dados de episodios no banco de dados

--- src/main/java/br/com/alura/screenmatch/model/Episodio.java
+++ src/main/java/br/com/alura/screenmatch/model/Episodio.java
@@ -19,6 +19,8 @@ public class Episodio {
     @ManyToOne
     private Serie serie;
 
+    public Episodio (){}
+
     public Episodio(Integer numeroTemporada, DadosEpisodio dadosEpisodio){
         this.temporadas = numeroTemporada;
         this.titulo = dadosEpisodio.titulo();
